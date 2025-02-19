6e0de1aae2cd76eb67b9b12c0642072c524bbba7
mnuddindev <inadislam@gmail.com>
2025-02-19 19:41:02 +0600

    ♻️ Remove unused PostsController code and clean up build errors log

--- controllers/posts/posts.go
+++ controllers/posts/posts.go
@@ -1,13 +1,1 @@
 package posts
-
-import "github.com/mnuddindev/devpulse/pkg/services"
-
-type PostsController struct {
-	postSystem *services.UserSystem
-}
-
-func NewUserController(userSystem *services.UserSystem) *PostsController {
-	return &PostsController{
-		postSystem: userSystem,
-	}
-}
