ad7adfb103df36a91f0ef5ec27fd1878a33b6229
Md Nazim Uddin <inadislam@gmail.com>
2025-04-10 02:14:34 +0600

    ✨ Implement CreateTag function to create and store new tags with validation and caching

--- internal/models/posts/tag.go
+++ internal/models/posts/tag.go
@@ -1,10 +1,15 @@
 package models
 
 import (
+	"context"
+	"encoding/json"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	user "github.com/mnuddindev/devpulse/internal/models/user"
+	storage "github.com/mnuddindev/devpulse/pkg/redis"
+	"github.com/mnuddindev/devpulse/pkg/utils"
 	"gorm.io/gorm"
 )
 
@@ -38,3 +43,40 @@ type Tag struct {
 
 // TagOptions defines a function type that takes a pointer to a Tag and modifies it.
 type TagOption func(*Tag)
+
+// CreateTag creates a new Tag instance with the provided options.
+func CreateTag(ctx context.Context, rclient *storage.RedisClient, db *gorm.DB, tag *Tag) error {
+	tag.Name = strings.TrimSpace(tag.Name)
+	tag.Slug = strings.ToLower(strings.TrimSpace(tag.Slug))
+	if tag.Name == "" || tag.Slug == "" {
+		return utils.NewError(utils.ErrBadRequest.Code, "Tag name and slug are required")
+	}
+
+	if !tag.IsApproved {
+		tag.IsApproved = false
+	}
+
+	err := db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		var existingByName Tag
+		if err := tx.Where("name = ?", tag.Name).First(&existingByName).Error; err == nil {
+			return utils.NewError(utils.ErrBadRequest.Code, "Tag name already exists")
+		} else if err != gorm.ErrRecordNotFound {
+			return utils.WrapError(err, utils.ErrInternalServerError.Code, "Failed to check tag name")
+		}
+
+		if err := tx.Create(tag).Error; err != nil {
+			return utils.WrapError(err, utils.ErrInternalServerError.Code, "Failed to create tag")
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	tagData, _ := json.Marshal(tag)
+	rclient.Set(ctx, "tag:"+tag.ID.String(), tagData, 24*time.Hour)
+	rclient.Set(ctx, "tag:slug:"+tag.Slug, tagData, 24*time.Hour)
+
+	return nil
+}
